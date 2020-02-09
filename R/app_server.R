4e9d269106534e0ceb0aea43218eae7ec0da20a6
Sharla Gelfand <sharla.gelfand@gmail.com>
2020-02-08 23:47:44 -0500

    Handle new dmc_df S3 method

--- R/app_server.R
+++ R/app_server.R
@@ -2,7 +2,8 @@
 app_server <- function(input, output, session) {
   # List the first level callModules here
   output$dmc <- shiny::renderImage({
-    tmpfile <- dmc::dmc(input$color, n = input$n, method = input$method)[["viz"]] %>%
+    tmpfile <- dmc::dmc(input$color, n = input$n, method = input$method) %>%
+      attr("visualization") %>%
       magick::image_write(tempfile(fileext = "jpg"), format = "jpg")
     list(src = tmpfile, contentType = "image/jpeg")
   })
